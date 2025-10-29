efed3ee3ca2f4e456022f726ff87d9169fe98d4b
VelpaChallenger <velpachallenger@gmail.com>
2025-10-29 19:00:13 -0300

    refactor: tidy things a bit before going back to main
    Woo-hoo! We're getting there!

--- Character.py
+++ Character.py
@@ -1,25 +1,14 @@
 from Anim import *
 
-CHARACTER_PALETTE_FILEPATH = "sub_zero.pal"
-CHARACTER_CHR_FILEPATH = "sub_zero.chr" #Only 2K for now.
-CHARACTER_CHR_PALETTE_FILEPATH = "sub_zero.chr.pal"
-CHARACTER_FRAMES_FILEPATH = "sub_zero_frame_01.frame"
-
 class Character:
 
     def __init__(self, createanims, character_name):
+        self.name = character_name #Not sure if we'll need it but... maybe for display purposes? We could add a dictionary or something... anyways, don't want to include too much in this final commit before going back to main.
         self.createanims = createanims
-        self.palette = self.get_palette("subzero")
+        self.palette = self.get_palette(character_name)
         chr_ids = [0x9C] #When I add more characters, there'll be more and they'll be obtained after we parse the anim file.
         self.chr_palettes, self.chrs = self.get_chrs_and_palettes(character_name, chr_ids) #Whatever, the and makes it clear. Any other name to include both just doesn't make it clear.
         self.frames = self.get_frames(character_name, [0x00, 0x01, 0x02, 0x03, 0x04, 0x05])
-        #self.chr_palettes = {}
-        #self.chr_palette = self.get_chr_palette("subzero")
-        #self.chr_palettes
-        #self.chr_palettes.append(self.chr_palette) #For now, we'll do the loop eventually. Yes yes eventually you heard my word!
-        #self.chrs = [] #And then you can access each chr_palette and chr by bank ID. Super awesome and easy to follow!
-        #self.chr = self.get_chr("subzero")
-        #self.chrs.append(self.chr) #Yes, I could have like a list of both chr and chr_palettes but here I prefer it this way, it feels clearer. Like the alternative would be self.banks or something and then per key (bank ID) 
 
     def get_chrs_and_palettes(self, name, chr_ids): #Yeah, after all we're kinda going to do the loop thing now, though not entirely but leaving the bases.
         chr_palettes = {}
@@ -32,17 +21,17 @@ class Character:
         return chr_palettes, chrs
 
     def get_palette(self, name):
-        with open(CHARACTER_PALETTE_FILEPATH, "rb") as character_pal:
+        with open(f"{self.createanims.root_dir}/{name}/pal/{name}_usual.pal", "rb") as character_pal:
             palette = list(character_pal.read())
         return palette
 
     def get_chr_palette(self, name, chr_id): #When we include absolutely every CHR_PALETTE with its corresponding CHR (I used uppercase cause I wanted to), we can iterate over a loop and then append to a chr_palettes and character_chrs. Awesome.
-        with open(CHARACTER_CHR_PALETTE_FILEPATH, "rb") as character_chr_pal:
+        with open(f"{self.createanims.root_dir}/{name}/chr/{name}_chr_palette_{chr_id:03d}.chr.pal", "rb") as character_chr_pal:
             chr_palette = list(character_chr_pal.read())
         return chr_palette
 
     def get_chr(self, name, chr_id):
-        with open(CHARACTER_CHR_FILEPATH, "rb") as character_chr:
+        with open(f"{self.createanims.root_dir}/{name}/chr/{name}_chr_{chr_id:03d}.chr", "rb") as character_chr:
             character_chr = list(character_chr.read())
         return character_chr #Added character cause otherwise chr, you know, reserved keyword.
 
--- create_anims.py
+++ create_anims.py
@@ -1,17 +1,12 @@
 from CreateAnims import *
 from Character import *
 
-#PATHS (here for now, all in one place)
-
-character_bank = "bank_sub_zero_copy.asm"
-
 def load_game_anims(createanims): #Another idea was to have a call to this in init_state, which makes sense considering we're initializing values. But then I would sort of have a circular dependency. I would have to put that stuff in yet another file, and I kinda like it here in the main file, so to speak. So that's why I ended up doing this way. It still makes sense: all the data has to go to createanims.
     character = Character(createanims, "sub_zero") #load_character("subzero") #What character to load. It's a prefix of sorts that might be needed for every character to get the right files? But I need to change how that is displayed so that users won't see pure keys let's say, or these IDs.
     createanims.characters.append(character)
 
 def main():
     createanims = CreateAnims()
-    #createanims.chr_palette = get_chr_palette() #This is probably the only one that doesn't depend on which character is being loaded.
     createanims.root_dir = "../characters"
     load_game_anims(createanims)
     createanims.current_character = 0 #ID-based.

cecda2b365eecc647213d75d632b9feca6a96807
VelpaChallenger <velpachallenger@gmail.com>
2025-11-06 21:27:36 -0300

    feat: insert initial anim values

--- create_anims.py
+++ create_anims.py
@@ -11,7 +11,13 @@ def main():
     load_game_anims(createanims)
     createanims.current_character = 0 #ID-based.
     createanims.current_chr_bank = next(iter(createanims.characters[0].chrs)) #Though I don't really like having to use next and iter. But meh. To get first key. #More generic this way. Won't matter whoever is first character. #0x9C
+    createanims.chr_entry.insert(0, str(createanims.current_chr_bank))
+    createanims.current_anim = 0x00
+    createanims.anim_entry.insert(0, str(createanims.current_anim))
+    createanims.current_frame = 0x00
+    createanims.frame_entry.insert(0, str(createanims.current_frame))
     createanims.current_frame_id = 0x01
+    createanims.frame_id_entry.insert(0, str(createanims.current_frame_id))
     createanims.refresh_UI()
     createanims.root.mainloop()
 main()

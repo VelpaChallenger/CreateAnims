fcf7e4ccac0561b2e349610ed1c82e3cfbe8a2ea
VelpaChallenger <velpachallenger@gmail.com>
2025-11-13 15:49:37 -0300

    refactor: move play_anim to Anim
    Button contains only the commands for buttons

--- Anim.py
+++ Anim.py
@@ -313,6 +313,24 @@ class Anim: #Yes this could be AnimUtils. Or maybe FrameUtils, come to think of 
         self.createanims.character_left_arrow.configure(state="normal")
         self.createanims.character_right_arrow.configure(state="normal")
 
+    def play_anim(self, event=None): #The one that runs over and over. Then the init code runs only once. Alternative is to create StringVar and then trace and pass this. But you still get two functions. I like more this.
+        import tkinter
+        if not self.createanims.in_play_anim:
+            return #And the chain stops.
+        self.createanims.png_img.clear()
+        character = self.createanims.characters[self.createanims.current_character]
+        frame_id = character.anims[self.createanims.current_anim].frame_ids[self.createanims.current_frame]
+        img = tkinter.PhotoImage(file=f"{self.createanims.root_dir}/{character.name}/images/{character.name}_frame_{frame_id:03d}.png") #(self.createanims.tiles_images[0].pre_tkimg.resize((16, 16)))
+        self.createanims.png_img.append(img) #Say no to garbage collection of PhotoImage.
+        frame = character.frames[frame_id]
+        self.createanims.play_anim_label.place(x=375+(frame.metadata.x_offset*2), y=200 - (frame.metadata.y_offset*2) - (16*frame.metadata.y_length))
+        self.createanims.play_anim_label.configure(image=img)
+        if self.createanims.current_frame == len(character.anims[self.createanims.current_anim].frame_ids) - 1:
+            self.createanims.current_frame = 0
+        else:
+            self.createanims.current_frame += 1
+        self.createanims.root.after(47, self.play_anim) #I think 47 is the magical number. Looks really, really good.
+
     def generate_png_from_anim_frames(self, character):
         from generate_anim_images import refresh_chr, generate_png
         for frame_id in character.anims[self.createanims.current_anim].frame_ids:

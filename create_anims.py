c41e32c0455821e5cab7364ab75363958b9fff9d
VelpaChallenger <velpachallenger@gmail.com>
2025-11-14 10:19:13 -0300

    feat: get physics from files if they exist

--- create_anims.py
+++ create_anims.py
@@ -4,10 +4,24 @@ from CreateAnims import *
 from Character import *
 
 def load_game_anims(createanims): #Another idea was to have a call to this in init_state, which makes sense considering we're initializing values. But then I would sort of have a circular dependency. I would have to put that stuff in yet another file, and I kinda like it here in the main file, so to speak. So that's why I ended up doing this way. It still makes sense: all the data has to go to createanims.
+    get_physics(createanims) #Could use return but... meh, this will do.
     characters_name_list = os.listdir(createanims.root_dir)
     for character_name in characters_name_list:
+        if character_name == "physics": #Not a character actually!
+            continue
         character = Character(createanims.root_dir, character_name)
         createanims.characters.append(character)
+
+def get_physics(createanims): #Yup. This will happen here. #Also yeah, CreateAnims can run without physics. I was going to add get_physics_if_any but, considering I may extend this to other kind of elements... for now I'll leave it as it is.
+    physics_path = f"{createanims.root_dir}/physics"
+    if not os.path.isdir(physics_path): #No or trickery this time around.
+        return
+    physics_name_list = os.listdir(physics_path)
+    physics_ids = [int(physics_filename.split(".physics")[0][-3:]) for physics_filename in physics_name_list] #As always, careful about .bak files and stuff. Will add this to docs.
+    for physics_id in physics_ids:
+        with open(f"{physics_path}/physics_{physics_id:03d}.physics", "rb") as physics_file:
+            physics = list(physics_file.read()) #In this case, we won't use an object. This will do.
+        createanims.physics_list.append(physics)
 
 def main():
     createanims = CreateAnims()

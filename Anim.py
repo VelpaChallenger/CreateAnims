df33d32906d3ecd5a60e0a12e2a8b3ef8b167a93
VelpaChallenger <velpachallenger@gmail.com>
2025-11-21 17:38:51 -0300

    fix: edit is still available during play anim
    Oops. There. Fixed.

--- Anim.py
+++ Anim.py
@@ -637,6 +637,10 @@ class Anim: #Yes this could be AnimUtils. Or maybe FrameUtils, come to think of 
         self.createanims.character_right_arrow.configure(state="disabled")
         self.createanims.edit_physics_button.configure(state="disabled")
         self.createanims.menu_bar.entryconfigure("File", state="disabled")
+        self.createanims.menu_bar.entryconfigure("Edit", state="disabled")
+        self.createanims.edit_menu.entryconfigure("Undo", state="disabled")
+        self.createanims.edit_menu.entryconfigure("Redo", state="disabled")
+        self.createanims.edit_menu.entryconfigure("Switch UndoRedo branch", state="disabled")
         self.createanims.menu_bar.entryconfigure("Anim", state="disabled")
         self.createanims.menu_bar.entryconfigure("Import", state="disabled")
 
@@ -670,6 +674,10 @@ class Anim: #Yes this could be AnimUtils. Or maybe FrameUtils, come to think of 
         self.createanims.character_right_arrow.configure(state="normal")
         self.createanims.edit_physics_button.configure(state="normal")
         self.createanims.menu_bar.entryconfigure("File", state="normal")
+        self.createanims.menu_bar.entryconfigure("Edit", state="normal")
+        self.createanims.edit_menu.entryconfigure("Undo", state="normal") #I need to disable and reenable those on-demand. Mainly because the logic depends on the status of those, but honestly, I still prefer it this way.
+        self.createanims.edit_menu.entryconfigure("Redo", state="normal")
+        self.createanims.edit_menu.entryconfigure("Switch UndoRedo branch", state="normal")
         self.createanims.menu_bar.entryconfigure("Anim", state="normal")
         self.createanims.menu_bar.entryconfigure("Import", state="normal")
 

72c7cb954be7e846dcf4aaf4c739277b5f8ac87e
VelpaChallenger <velpachallenger@gmail.com>
2025-12-09 01:28:25 -0300

    feat: add try-except blocks for CreateAnimsUpdater
    CreateAnimsUpdater will rarely update. Even when performing an update, only the executable will be updated, well and potentially the README.html, everything else won't, I mean just the updater won't. So from this version on, I'd like to have a lot of try-except what is also called error handling and add also that if there's an error, maybe it's because we do need to download directly from the repo. Alright!

--- CreateAnimsUpdater.py
+++ CreateAnimsUpdater.py
@@ -4,25 +4,60 @@ import os
 import subprocess
 import tkinter
 import threading
-from tkinter import ttk
+from tkinter import ttk, messagebox
 
 CHUNK_SIZE = 4096
 
 def thread_download():
-    with requests.get("https://github.com/VelpaChallenger/CreateAnims/releases/latest/download/CreateAnims.zip", stream=True) as request:
-        download_bar['maximum'] = int(request.headers.get('Content-Length'))
-        request.raise_for_status()
-        with open("new_create_anims.zip", "wb") as new_create_anims_zip:
-            for chunk in request.iter_content(chunk_size=CHUNK_SIZE):
-                new_create_anims_zip.write(chunk)
-                download_bar['value'] += CHUNK_SIZE
+    try:
+        with requests.get("https://github.com/VelpaChallenger/CreateAnims/releases/latest/download/CreateAnims.zip", stream=True) as request:
+            download_bar['maximum'] = int(request.headers.get('Content-Length'))
+            request.raise_for_status()
+            with open("new_create_anims.zip", "wb") as new_create_anims_zip:
+                for chunk in request.iter_content(chunk_size=CHUNK_SIZE):
+                    new_create_anims_zip.write(chunk)
+                    download_bar['value'] += CHUNK_SIZE
+    except requests.exceptions.ConnectionError:
+        messagebox.showerror(title="Unable to download update", message="Unable to download update. Please confirm you have an stable internet connection.")
+        root.after(10, cancel_download)
+        return
+    except requests.exceptions.HTTPError:
+        messagebox.showerror(title="Unable to download update", message="Unable to download update. GitHub might be down, or the updater is old enough that the download URL is no longer valid. Please consider downloading directly from CreateAnims repo.") #I remember all the times I read stuff like this, what do you mean I don't have an STABLE internet connection, it's more stable than you are you...!! lol. Oh this is a comment right? Which means other people are gonna read it? I mean. Whatever.
+        root.after(10, cancel_download)
+        return
+    except Exception as e:
+        messagebox.showerror(title="Unable to download update", message=f"Unable to download update. Unknown exception: {e}")
+        root.after(10, cancel_download)
+        return
     root.after(10, post_download) #Looks hacky and maybe it is? But works. Destroy cannot happen in this thread as I originally thought but still gave it the try yeah now I know for sure.
 
 def post_download():
-    with zipfile.ZipFile("new_create_anims.zip", 'r') as zip_file:
-        zip_file.extractall(".", members=(member for member in zip_file.namelist() if member != "CreateAnimsUpdater.exe")) #CreateAnimsUpdater is running. Don't extract it, you won't be able to.
-    os.remove("new_create_anims.zip")
-    subprocess.Popen("CreateAnims.exe") #With this, the updater closes and ends. #Also it has to be in this order, if you run destroy first, this never runs. Like priorities and such. #Nah it can happen in any order the important part is that the destroy doesn't happen in a thread.
+    try:
+        with zipfile.ZipFile("new_create_anims.zip", 'r') as zip_file:
+            zip_file.extractall(".", members=(member for member in zip_file.namelist() if member != "CreateAnimsUpdater.exe")) #CreateAnimsUpdater is running. Don't extract it, you won't be able to.
+    except PermissionError as e:
+        messagebox.showerror(title="Unable to extract files", message=f"Unable to extract files. Exception details: {e}. Please avoid opening files while the update is in progress.") #I remember all the times I read stuff like this, what do you mean I don't have an STABLE internet connection, it's more stable than you are you...!! lol. Oh this is a comment right? Which means other people are gonna read it? I mean. Whatever.
+        root.destroy() #We're no longer in the thread.
+        return
+    except Exception as e:
+        messagebox.showerror(title="Unable to extract files", message=f"Unable to extract files. Unknown exception: {e}.")
+        root.destroy() #We're no longer in the thread.
+        return
+    try: #Let's make it as robust as possible from the beginning, so that we don't have to make changes to it except for IDs changing and stuff like that, and we'll be able to inform them.
+        os.remove("new_create_anims.zip")
+    except Exception as e:
+        messagebox.showerror(title="Unable to remove downloaded zip", message=f"Unable to remove downloaded zip. Unknown exception: {e}.")
+        root.destroy() #We're no longer in the thread.
+        return
+    try:
+        subprocess.Popen("CreateAnims.exe") #With this, the updater closes and ends. #Also it has to be in this order, if you run destroy first, this never runs. Like priorities and such. #Nah it can happen in any order the important part is that the destroy doesn't happen in a thread.
+    except Exception as e:
+        messagebox.showerror(title="Unable to reopen CreateAnims", message=f"Unable to reopen CreateAnims. Unknown exception: {e}.")
+        root.destroy() #We're no longer in the thread.
+        return
+    root.destroy()
+
+def cancel_download():
     root.destroy()
 
 root = tkinter.Tk()
